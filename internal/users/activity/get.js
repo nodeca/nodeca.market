62c67ed765d683da53dfa7b02ff395292e46c03f
Alex Kocharin <alex@kocharin.ru>
2019-05-14 11:54:57 +0300

    Fix activity counter display

--- internal/users/activity/get.js
+++ internal/users/activity/get.js
@@ -14,11 +14,13 @@
 module.exports = function (N, apiPath) {
 
   N.wire.on(apiPath, { parallel: true }, async function activity_get_market(data) {
-    data.count += (await Promise.all([
+    let count = (await Promise.all([
       N.models.market.UserItemOfferCount.get(data.user_id, data.current_user_info),
       N.models.market.UserItemOfferArchivedCount.get(data.user_id, data.current_user_info),
       N.models.market.UserItemWishCount.get(data.user_id, data.current_user_info),
       N.models.market.UserItemWishArchivedCount.get(data.user_id, data.current_user_info)
     ])).reduce((a, b) => a + b, 0);
+
+    data.count += count;
   });
 };
