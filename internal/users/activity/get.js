ad2e6d9b7a6aed7d3a8f424fb4135f9034d2271e
Alex Kocharin <alex@kocharin.ru>
2019-05-14 19:39:17 +0300

    Allow block queries for activity counters

--- internal/users/activity/get.js
+++ internal/users/activity/get.js
@@ -11,16 +11,24 @@
 'use strict';
 
 
+const _ = require('lodash');
+
+
 module.exports = function (N, apiPath) {
 
   N.wire.on(apiPath, { parallel: true }, async function activity_get_market(data) {
-    let count = (await Promise.all([
+    let counts = await Promise.all([
       N.models.market.UserItemOfferCount.get(data.user_id, data.current_user_info),
       N.models.market.UserItemOfferArchivedCount.get(data.user_id, data.current_user_info),
       N.models.market.UserItemWishCount.get(data.user_id, data.current_user_info),
       N.models.market.UserItemWishArchivedCount.get(data.user_id, data.current_user_info)
-    ])).reduce((a, b) => a + b, 0);
+    ]);
 
-    data.count += count;
+    if (Array.isArray(data.count)) {
+      let zipped = _.zip(...counts);
+      data.count = data.count.map((c, i) => c + _.sum(zipped[i]));
+    } else {
+      data.count += _.sum(counts);
+    }
   });
 };
