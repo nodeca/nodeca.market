d4a3e34e7632bb1ee47ff2ae1010b3c5d87b03b6
Alex Kocharin <alex@kocharin.ru>
2019-08-29 04:38:49 +0300

    Create update_url debouncer lazily on first call

--- client/market/user/buy_closed/buy_closed.js
+++ client/market/user/buy_closed/buy_closed.js
@@ -49,50 +49,51 @@ function load(start, direction) {
 }
 
 
-// Use a separate debouncer that only fires when user stops scrolling,
-// so it's executed a lot less frequently.
-//
-// The reason is that `history.replaceState` is very slow in FF
-// on large pages: https://bugzilla.mozilla.org/show_bug.cgi?id=1250972
-//
-let update_url = _.debounce((item, index, item_offset) => {
-  let href, state;
-
-  if (item) {
-    state = {
-      hid:    $(item).data('item-hid'),
-      offset: item_offset
-    };
-  }
-
-  // save current offset, and only update url if current_item is different
-  if (pageState.current_offset !== index) {
-    let $query = {};
-
-    if (item) $query.from = $(item).data('item-hid');
-
-    href = N.router.linkTo('market.user.buy_closed', {
-      user_hid: pageState.hid,
-      $query
-    });
-
-    if ((pageState.current_offset >= 0) !== (index >= 0) && !pageState.tag) {
-      $('meta[name="robots"]').remove();
-
-      if (index >= 0) {
-        $('head').append($('<meta name="robots" content="noindex,follow">'));
-      }
-    }
-
-    pageState.current_offset = index;
-  }
-
-  N.wire.emit('navigate.replace', { href, state })
-        .catch(err => N.wire.emit('error', err));
-}, 500);
-
+let update_url;
 
 function on_list_scroll(item, index, item_offset) {
+  // Use a separate debouncer that only fires when user stops scrolling,
+  // so it's executed a lot less frequently.
+  //
+  // The reason is that `history.replaceState` is very slow in FF
+  // on large pages: https://bugzilla.mozilla.org/show_bug.cgi?id=1250972
+  //
+  update_url = update_url || _.debounce((item, index, item_offset) => {
+    let href, state;
+
+    if (item) {
+      state = {
+        hid:    $(item).data('item-hid'),
+        offset: item_offset
+      };
+    }
+
+    // save current offset, and only update url if current_item is different
+    if (pageState.current_offset !== index) {
+      let $query = {};
+
+      if (item) $query.from = $(item).data('item-hid');
+
+      href = N.router.linkTo('market.user.buy_closed', {
+        user_hid: pageState.hid,
+        $query
+      });
+
+      if ((pageState.current_offset >= 0) !== (index >= 0) && !pageState.tag) {
+        $('meta[name="robots"]').remove();
+
+        if (index >= 0) {
+          $('head').append($('<meta name="robots" content="noindex,follow">'));
+        }
+      }
+
+      pageState.current_offset = index;
+    }
+
+    N.wire.emit('navigate.replace', { href, state })
+          .catch(err => N.wire.emit('error', err));
+  }, 500);
+
   N.wire.emit('common.blocks.navbar.blocks.page_progress:update', {
     current: index + 1 // `+1` because offset is zero based
   }).catch(err => N.wire.emit('error', err));
@@ -177,7 +178,9 @@ N.wire.on('navigate.done:' + module.apiPath, function page_setup(data) {
 N.wire.on('navigate.exit:' + module.apiPath, function page_teardown() {
   scrollable_list.destroy();
   scrollable_list = null;
-  update_url.cancel();
+
+  if (update_url) update_url.cancel();
+
   pageState = {};
 });
 
