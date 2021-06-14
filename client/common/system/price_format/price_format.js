2f40bab0da0b01d06614981fcfa464b65628f2f1
Alex Kocharin <alex@kocharin.ru>
2021-06-14 14:18:25 +0300

    Wrap market prices in spans

--- client/common/system/price_format/price_format.js
+++ client/common/system/price_format/price_format.js
@@ -6,23 +6,6 @@
 const price_format = require('nodeca.market/lib/app/price_format');
 
 
-function price_helper(value, currency, user_currency, rate) {
-  let symbol_real = N.runtime.t.exists('market.currencies.' + currency + '.sign') ?
-                    N.runtime.t('market.currencies.' + currency + '.sign') :
-                    currency;
-
-  if (user_currency && user_currency !== currency && rate && rate > 0) {
-    let symbol_base = N.runtime.t.exists('market.currencies.' + user_currency + '.sign') ?
-                      N.runtime.t('market.currencies.' + user_currency + '.sign') :
-                      user_currency;
-
-    return price_format(value * rate, symbol_base, true) + ' (' + price_format(value, symbol_real) + ')';
-  }
-
-  return price_format(value, symbol_real);
-}
-
-
 N.wire.once('init:assets', function price_helper_register() {
-  N.runtime.render.helpers.price = price_helper;
+  N.runtime.render.helpers.price = price_format;
 });
