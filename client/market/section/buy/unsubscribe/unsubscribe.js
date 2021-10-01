e33d8d86044087455a9b42aff90d621da51c995b
Alex Kocharin <alex@kocharin.ru>
2021-10-01 21:21:01 +0300

    Add dialogs for subscribing to market sections

---
+++ client/market/section/buy/unsubscribe/unsubscribe.js
@@ -0,0 +1,21 @@
+'use strict';
+
+
+N.wire.once('navigate.done:' + module.apiPath, function page_once() {
+
+  // Edit subscription button handler
+  //
+  N.wire.on(module.apiPath + ':edit', function edit_subscription(data) {
+    let hid = data.$this.data('hid');
+    let params = { subscription: data.$this.data('subscription') };
+
+    return Promise.resolve()
+      .then(() => N.wire.emit('market.section.buy.subscription', params))
+      .then(() => N.io.rpc('market.section.buy.subscribe', { section_hid: hid, type: params.subscription }))
+      .then(() => {
+        data.$this.replaceWith(
+          N.runtime.render(module.apiPath + '.button', { section: { hid }, subscription: params.subscription })
+        );
+      });
+  });
+});
